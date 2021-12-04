ee820b72e038f51221096d9e84e64d9fd7738aae
JSolis2308 <jossgeme1@gmail.com>
2021-12-04 17:01:48 -0600

    Añadiendo un script de C para la creación del código fuente para la recreación del juego Tic Tac Toe.

---
+++ ProyectoC_Grupo_8.c
@@ -0,0 +1,14 @@
+/*
+Proyecto de programacion para el curso Programacion Bajo Plataformas Abiertas
+de la Universidad de Costa Rica en lenguaje de programacion C destinado a la
+recreacion del videojuego Tic Tac Toe (Juego de Gato). Para la creación de este
+código fue utilizada la distribución de Ubuntu 18.04/20.04.
+
+Autores: Johan Solis Soto  C07686
+         Sergio Camacho B91476
+	 Mario Navarro Bejarano B75398
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
